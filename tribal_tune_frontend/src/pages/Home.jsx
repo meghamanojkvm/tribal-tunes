e18e1e542d1dcbf1dd69d5ef4ecc64a6f1f72416
arpitkashyap22 <64071429+arpitkashyap22@users.noreply.github.com>
2024-05-26 19:51:30 +0530

    remove titleheader

--- tribal_tune_frontend/src/pages/Home.jsx
+++ tribal_tune_frontend/src/pages/Home.jsx
@@ -3,7 +3,6 @@ import { TitleHeader } from "../components/TitleHeader"
 export const Home  = () => {
     return(
         <div>
-            <TitleHeader></TitleHeader>
             this is home page
         </div>
     )
