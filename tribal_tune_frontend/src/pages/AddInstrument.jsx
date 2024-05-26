33e048396f44f510616bd40ca770c064d15874ee
arpitkashyap22 <64071429+arpitkashyap22@users.noreply.github.com>
2024-05-26 19:54:23 +0530

    remove TitleHeader

--- tribal_tune_frontend/src/pages/AddInstrument.jsx
+++ tribal_tune_frontend/src/pages/AddInstrument.jsx
@@ -1,9 +1,8 @@
-import { TitleHeader } from "../components/TitleHeader"
+
 
 export const AddInstrument = () =>{
     return(
         <div>
-            <TitleHeader></TitleHeader>
             Add instrument details
         </div>
     )
--- tribal_tune_frontend/src/pages/ShowInstrument.jsx
+++ tribal_tune_frontend/src/pages/ShowInstrument.jsx
@@ -1,4 +1,4 @@
-import { TitleHeader } from "../components/TitleHeader"
+
 
 import React from 'react';
 
