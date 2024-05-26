64fe0230978969158d6674bb6a528aa2eb0fcbce
Harsh Kumar <154247733+harshk707070@users.noreply.github.com>
2024-05-26 20:13:22 +0530

    landing page added

--- tribal_tune_frontend/src/pages/Home.jsx
+++ tribal_tune_frontend/src/pages/Home.jsx
@@ -1,10 +1,21 @@
 import { TitleHeader } from "../components/TitleHeader"
+import { useNavigate } from "react-router-dom";
 
-export const Home  = () => {
-    return(
+export const Home = () => {
+    const navigate = useNavigate()
+
+    return (
         <div>
-            <TitleHeader></TitleHeader>
-            this is home page
+            <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+                <h1 className="text-5xl font-bold mb-4 text-blue-600">Tribal Tune</h1>
+                <p className="text-xl mb-8 text-gray-800">Experience the rhythm of the Indian tribe with our unique blend of traditional and contemporary music.</p>
+                <div className="flex space-x-4">
+                    <button className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() =>{
+                        navigate("/instruments");
+                    }}>Listen Now</button>
+                    <button className="bg-white hover:bg-gray-100 text-gray-800 font-bold py-2 px-4 border border-gray-400 rounded">Learn More</button>
+                </div>
+            </div>
         </div>
     )
 }
