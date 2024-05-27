330ea9fd5d651ef426aa3f5eb08238819543425d
arpitkashyap22 <64071429+arpitkashyap22@users.noreply.github.com>
2024-05-27 09:26:59 +0530

    update add button

--- tribal_tune_frontend/src/components/Card.jsx
+++ tribal_tune_frontend/src/components/Card.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { UserProvider, useUser } from "../lib/context/user";
+import { useNavigate } from "react-router-dom";
 
 export const Card = ({ title, description, imageUrl, profileUrl }) => {
   const user = useUser();
+  const navigate = useNavigate();
+
   return (
     <div className="relative h-[300px] w-[300px] rounded-md my-4">
       <img
@@ -13,12 +16,12 @@ export const Card = ({ title, description, imageUrl, profileUrl }) => {
       <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
 
       {/* add button change it's position */}
-      {user.current ? <div><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+      {user.current ? <div onClick={() => navigate("/addinstrument")}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
         <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
       </svg>
       </div> : null}
       {/* add button ends */}
-      
+
       <div className="absolute bottom-4 left-4 text-left">
         <h1 className="text-lg font-semibold text-white">{title}</h1>
         <p className="mt-2 text-sm text-gray-300">{description}</p>
