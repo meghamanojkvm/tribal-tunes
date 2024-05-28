7053f37426e526e8e377887b60c9badf5cab3a49
Trupti_Walunjkar <truptiwalunjkar@gmail.com>
2024-05-28 13:12:05 +0530

    add audio

--- tribal_tune_frontend/src/App.jsx
+++ tribal_tune_frontend/src/App.jsx
@@ -6,12 +6,8 @@ import { Home } from "./pages/Home";
 import { AllInstrument } from "./pages/AllInstruments";
 import { AddInstrument } from "./pages/AddInstrument";
 import { ShowInstrument } from "./pages/ShowInstrument";
-<<<<<<< HEAD
-// import { ShowPiano } from './pages/ShowPiano';
-=======
+
 import { AboutUs } from "./pages/AboutUs";
->>>>>>> 26d4ae0529c00c33d89e5b9899a708ab15f9469c
- // Ensure this is imported correctly
 
 
 function App() {
@@ -24,14 +20,10 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/" element={<Home />} />
           <Route path="/addinstrument" element={<AddInstrument />} />
-<<<<<<< HEAD
-          <Route path="/instruments/:id" element={<ShowInstrument />} /> 
-          {/* <Route path="/piano" component={<ShowPiano />} /> */}
-=======
+
           <Route path="/instruments/:id" element={<ShowInstrument />} />
           <Route path="/aboutUs" element={<AboutUs/>} />
 
->>>>>>> 26d4ae0529c00c33d89e5b9899a708ab15f9469c
         </Routes>
       </BrowserRouter>
     </UserProvider>
