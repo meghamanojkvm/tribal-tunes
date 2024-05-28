749acbd52fee00e77752d5dd0ac7d94b4d71acc0
Trupti_Walunjkar <truptiwalunjkar@gmail.com>
2024-05-28 13:14:41 +0530

    audio update

--- tribal_tune_frontend/src/components/Audio.jsx
+++ tribal_tune_frontend/src/components/Audio.jsx
@@ -5,11 +5,6 @@ const Audio = ({ src }) => {
   return (
     <div className="mt-8 bg-[#f5f5dc]">
       <audio controls src={src} className="w-full bg-[#f5f5dc]">
-<<<<<<< HEAD
-        {/* <source src={src} type="audio/mpeg/mp3" />
-        Your browser does not support the audio element. */}
-=======
->>>>>>> 26d4ae0529c00c33d89e5b9899a708ab15f9469c
       </audio>
     </div>
   );
